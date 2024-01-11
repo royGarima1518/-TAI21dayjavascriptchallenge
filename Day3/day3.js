0ef09653ec0db5b457bdcb0a4cf9e4bf5beaa5a6
Garima Roy <145840106+royGarima1518@users.noreply.github.com>
2024-01-11 13:20:31 +0530

    Add files via upload

---
+++ Day3/day3.js
@@ -0,0 +1,10 @@
+const checkEvenOdd = (number) => {
+    if (number % 2 === 0) {
+        console.log(  number + " is Even");
+    } else {
+        console.log(  number + " is Odd");
+    }
+};
+
+const myNumber = 7;
+checkEvenOdd(myNumber);
