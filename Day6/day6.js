68ff0cdfd3b1eaf83878653c8bf0c3b3614e54d8
Garima Roy <145840106+royGarima1518@users.noreply.github.com>
2024-01-14 14:32:28 +0530

    Add files via upload

---
+++ Day6/day6.js
@@ -0,0 +1,14 @@
+/*Garima Roy*/
+
+function calculateSum(number1, number2) {
+    return number1 + number2;
+}
+
+const sum1 = calculateSum(5, 10);
+const sum2 = calculateSum(15, 20);
+const sum3 = calculateSum(-8, 6);
+
+
+console.log("Sum 1:", sum1);
+console.log("Sum 2:", sum2);
+console.log("Sum 3:", sum3);
