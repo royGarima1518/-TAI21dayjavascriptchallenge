7a83e7c73222fcf514540e88b075a00127ceec9f
Garima Roy <145840106+royGarima1518@users.noreply.github.com>
2024-01-15 20:50:42 +0530

    Add files via upload

---
+++ Day7/day7.js
@@ -0,0 +1,13 @@
+/*Garima Roy*/
+
+var person = {
+    name: "Garima Roy",
+    age: 21,
+    address: "Ram krishna nagar"
+  };
+  
+  
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("Address: " + person.address);
+  
