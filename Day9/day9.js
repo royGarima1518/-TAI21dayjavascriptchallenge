1773750dd9f1adc3349b61c72c616e4809d16807
Garima Roy <145840106+royGarima1518@users.noreply.github.com>
2024-01-18 20:25:19 +0530

    Add files via upload

---
+++ Day9/day9.js
@@ -0,0 +1,10 @@
+/*Garima Roy */
+document.addEventListener("DOMContentLoaded", function(){
+
+    const myButton = document.getElementById("myButton");
+
+    myButton.addEventListener("click", function()
+    {
+        alert("Day 9 of 21 day challenge!");
+    });
+});
