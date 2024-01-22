16d88f74fc4e39d415f0e3bf794de16682e59f01
Garima Roy <145840106+royGarima1518@users.noreply.github.com>
2024-01-22 22:50:01 +0530

    Add files via upload

---
+++ Day13/day13.js
@@ -0,0 +1,10 @@
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+  
+function sum(a, b) {
+    return "The sum of " + a + " and " + b + " is: " + (a + b);
+}
+  
+console.log(greet("Garima Roy"));
+console.log(sum(3, 5));
